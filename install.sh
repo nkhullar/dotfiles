f1e55ab16c6433fa766f23651bdda1c41a8756e8
Jinghao Shi <jhshi89@gmail.com>
2013-03-16 19:30:17 -0400

    Polish install.sh, now it first back up non-symlink dotfiles in /home/jhshi before make the link

--- install.sh
+++ install.sh
@@ -2,6 +2,19 @@
 
 for dotfile in _*
 do
+    source="${PWD}/$dotfile"
+    target="$HOME/${dotfile/_/.}"
+
+    if [ -e $target ]
+    then
+        if [ -L $target ]
+        then
+            rm -fv $target
+        else
+            mv -v $target $target.bk
+        fi
+    fi
+
     # make the symlink
-    ln -sf ${PWD}/$dotfile $HOME/${dotfile/_/.}
+    ln -sfv $source $target
 done
